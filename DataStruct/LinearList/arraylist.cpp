866784c3b473a80c1e38c5fa6d0717aaea46028b
zerone216 <zerone216@163.com>
2019-12-31 23:00:24 +0800

    update arraylist.cpp

--- DataStruct/LinearList/arraylist.cpp
+++ DataStruct/LinearList/arraylist.cpp
@@ -58,7 +58,6 @@ arrayList<T>::arrayList(int initialCapacity)
     listSize = 0;
 }
 
-
 template<class T>
 arrayList<T>::arrayList(const arrayList<T>& theList)
 {
@@ -67,4 +66,87 @@ arrayList<T>::arrayList(const arrayList<T>& theList)
     listSize = theList.listSize;
     element = new T[arrayLength];
     copy(theList.element, theList.element + listSize, element);
+}
+
+
+template<class T>
+void arrayList<T>::checkIndex(int theIndex) const
+{
+    //确定索引theIndex在 0 和 listSize - 1之间
+    if(theIndex < 0 || theIndex >= listSize)
+    {
+        ostringstream s;
+        s << "index = " << theIndex << " size = " << listSize;
+        throw illegalIndex(s.str());
+    }
+}
+
+template<class T>
+T& arrayList<T>::get(int theIndex) const
+{
+    checkIndex(theIndex);
+    return element[theIndex];
+}
+
+template<class T>
+int arrayList<T>::indexOf(const T& theElement) const
+{
+    int theIndex = (int) (find(element, element + listSize, theElement) - element);
+
+    if(theIndex == listSize)
+    {
+        return -1;
+    }
+    else
+    {
+        return theIndex;
+    }
+}
+
+template<class T>
+void arrayList<T>::erase(int theIndex)
+{
+    checkIndex(theIndex);
+    copy(element + theIndex + 1, element + listSize, element + theIndex);
+    element[--listSize].~T();
+}
+
+template<class T>
+void arrayList<T>::insert(int theIndex, const T& theElement)
+{
+    if(theIndex < 0 || theIndex > listSize)
+    {
+        ostringstream s;
+        s << "index = " << theIndex << " size = " << listSize;
+        throw illegalIndex(s.str());
+    }
+
+    if(listSize == arrayLength)
+    {
+        changLength1D(element, arrayLength, 2 * arrayLength);
+        arrayLength *= 2;
+    }
+
+    copy_backward(element + theIndex, element +listSize, element + listSize + 1);
+    element[theIndex] = theElement;
+    listSize ++;
+}
+
+template<class T>
+void arrayList<T>::output(cout->out) const
+{
+    copy(element, element + listSize, ostream_iterator<T>(cout, "  "));
+}
+
+/*
+ > FunctionName: << 
+ > Description: 重载运算符 <<
+ > Parameters: 
+ > Return: 
+ */
+template<class T>
+ostream& operator <<(ostream& out, const arrayList<T>& x)
+{
+    x.output(out);
+    return out;
 }
